30e86e24baf1c2cfa3c9bb8270fade5b8ece44b0
Princee <princec14552@gamil.com>
2026-04-05 15:47:39 +0545

    Added source code

---
+++ src/models.py
@@ -0,0 +1,35 @@
+class Asset:
+    def __init__(self,name,symbol):
+        self.name=name
+        self.symbol=symbol
+        self.current_price=0.0
+    def to_dict(self):
+        return {"name":self.name,
+                "symbol":self.symbol,
+                "price":self.current_price,
+                "type":self.__class__.__name__}
+
+class Stock(Asset):
+    def __init__(self,name,symbol,exchange):
+        super().__init__(name,symbol)
+        self.exchange=exchange
+
+
+class Metal(Asset):
+    def __init__(self,name,symbol,purity="24K"):
+        super().__init__(name,symbol)
+        self.purity=purity
+    def calculate_per_tola(self,usd_to_npr):
+        return f"Price per Tola: {(self.current_price*usd_to_npr)/2.66}"
+
+class Portfolio:
+    def __init__(self):
+        self.assets=[]
+    def add_asset(self,asset_obj):
+        '''To avoid any asset duplicates by checking if it is already in the list or not '''
+        exists=any(a.symbol==self.assets_obj.symbol for a in self.assets)
+        if not exists:
+            self.assets.append(asset_obj)
+            print()
+
+
