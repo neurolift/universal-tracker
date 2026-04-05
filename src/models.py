3fbfc606deacdcea71f965bce6d39de64a103c66
Princee <princec14552@gamil.com>
2026-04-05 18:38:58 +0545

    Updated Backend

--- src/models.py
+++ src/models.py
@@ -3,6 +3,8 @@ class Asset:
         self.name=name
         self.symbol=symbol
         self.current_price=0.0
+    def update_price(self,new_price):
+        self.current_price=new_price
     def to_dict(self):
         return {"name":self.name,
                 "symbol":self.symbol,
@@ -27,7 +29,7 @@ class Portfolio:
         self.assets=[]
     def add_asset(self,asset_obj):
         '''To avoid any asset duplicates by checking if it is already in the list or not '''
-        exists=any(a.symbol==self.assets_obj.symbol for a in self.assets)
+        exists = any(a.symbol == asset_obj.symbol for a in self.assets)
         if not exists:
             self.assets.append(asset_obj)
             print(f"Added {asset_obj.name} ({asset_obj.symbol}) to portfolio.")
@@ -36,7 +38,8 @@ class Portfolio:
     def update_all_prices(self,fetcher_function):
         '''This loops thorugh every assest and updtaes its price using your fetching logic'''
         for asset in self.assets:
-            new_price=fetcher_function(asset.symbol)
-            asset.update_price(new_price)
+            new_price_data=fetcher_function(asset.symbol)
+            if new_price_data:
+                asset.update_price(new_price_data['price'])
     def get_portfolio_data(self):
         return [asset.to_dict() for asset in self.assets]
